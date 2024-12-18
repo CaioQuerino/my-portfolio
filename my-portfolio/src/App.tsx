5eb06c74e255b791a3c7106f343228ec5ba33a64
Caio Querino <caioquerino04@gmail.com>
2024-12-18 19:05:06 -0300

    18-12-2024 : Adicionando max-width de 90rem ou 1440px.

--- my-portfolio/src/App.tsx
+++ my-portfolio/src/App.tsx
@@ -2,8 +2,8 @@ import { Footer } from './components/Footer.tsx'
 import { Header } from './components/Header.tsx'
 import { Main } from './components/Main.tsx'
 import { Nav } from './components/Nav.tsx'
-import './global.css'
 import { bio,  } from '../ts.config.tsx'
+import './global.module.css'
 
 export function App() {
  
