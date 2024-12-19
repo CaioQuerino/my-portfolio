cc3db07c6a4b9143a7fd166d522c64592a48897d
Caio Querino <caioquerino04@gmail.com>
2024-12-18 21:17:48 -0300

    18-12-2024 : Bug na responsividade.

--- my-portfolio/src/components/Footer.tsx
+++ my-portfolio/src/components/Footer.tsx
@@ -1,13 +1,13 @@
-import  './Footer.module.css'
+import styles from './Footer.module.css'
 
 import { OptionalProps } from '../../ts.config.tsx'
 
 export function Footer({ author, occupation }: OptionalProps ) {
     return (
-      <>
+      <footer className={styles.rodape}>
         <p>Created by {author} - {occupation}</p>
         <small>© 2024 All rights reserved.</small>
-      </>
+      </footer>
     );
   }
   
